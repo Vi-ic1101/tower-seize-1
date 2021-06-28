8a0c97f38df841726a20fafd217d5d42fdd416ce
Vi-ic1101 <83502402+Vi-ic1101@users.noreply.github.com>
2021-06-29 00:27:18 +0530

    Update Polygon.js

--- Polygon.js
+++ Polygon.js
@@ -1,24 +1,22 @@
 class Polygon{
 
-    constructor(x,y,w,h){
+    constructor(x,y){
         var polygon_options={
             restitution:0.4,
             friction:1.0,
             density:1
           }
-        this.x=x
-        this.y=y
-        this.w=w
-        this.h=h
-          this.polygon = Bodies.rectangle(x,y,w,h,polygon_options)
+      
+        
+          this.body = Bodies.rectangle(x,y,50,50,polygon_options)
           this.image=loadImage("polygon.png")
-          World.add(world,this.polygon);
+          World.add(world,this.body);
           
     }
     display(){
         noStroke(255);
         fill(255);
         imageMode(CENTER);
-        image(this.image,this.polygon.position.x,this.polygon.position.y,50,50);
+        image(this.image,this.body.position.x,this.body.position.y,50,50);
     }
 }
